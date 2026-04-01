f420dfc9b302c73e74079ba4eaf0358d360d3ac6
Alex Andriitsev <AlexAndritsev83@users.noreply.github.com>
2026-04-01 11:44:07 +0300

    Configure locale and input source settings
    Update locale settings and input source keybindings for UK.

--- modules/locale.sh
+++ modules/locale.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+sudo update-locale LANG=en_US.UTF-8 \
+LC_TIME=uk_UA.UTF-8 \
+LC_NUMERIC=uk_UA.UTF-8 \
+LC_MONETARY=uk_UA.UTF-8 \
+LC_MEASUREMENT=uk_UA.UTF-8
+
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Alt>Shift_L']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift>Alt_L']"
+gsettings set org.gnome.desktop.input-sources xkb-options "['grp:alt_shift_toggle']"
+
+sudo apt install -y hunspell-uk
